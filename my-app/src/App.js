5e7a4fee92aca49467087484321932907a840a18
NATNgoc <nguyenatn2003@gmail.com>
2024-10-04 14:27:36 +0700

    Add logo to header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Header } from './components/Header'
+import Header from './components/Header';
 import { Users } from './components/Users'
 import { DisplayBoard } from './components/DisplayBoard'
 import CreateUser from './components/CreateUser'
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -3,10 +3,10 @@ import React from 'react';
 function Header() {
   return (
     <header className="header">
-      <img src="/path/to/your/logo.png" alt="Logo" className="header-logo" />
+      <img src="/z5891241513756_922996e39d034ddc0fe8174d314d257b.jpg" alt="Logo" className="header-logo" />
       <h1>My React App</h1>
     </header>
   );
-}
+}   
 
 export default Header;
