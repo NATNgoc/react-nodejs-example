cda55ee2d366330dad679473fdb87818b1d14885
NATNgoc <nguyenatn2003@gmail.com>
2024-10-06 11:00:49 +0700

    Test auto 10/06/2024

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Header() {
   return (
     <header className="header">
-      <img src="/z5891241513756_922996e39d034ddc0fe8174d314d257b.jpg" alt="Logo" className="header-logo" />
       <h1>My React App</h1>
+      <img src="/z5891241513756_922996e39d034ddc0fe8174d314d257b.jpg" alt="Logo" className="header-logo" />
     </header>
   );
 }   
